6e5366a54257ae9bbab8d364e62ff790b3140ee5
«Anton» <hranitel-liks@mail.ru>
2022-09-07 20:31:58 +0700

    add HW001

---
+++ Tasks/HW001/Program.cs
@@ -0,0 +1,10 @@
+﻿// Показать натуральные числа от N до 1, N задано
+Console.Clear();
+
+void PrintNumber(int n)
+{
+    if (n < 1) return;
+    PrintNumber(n - 1);
+    Console.Write(n + " ");
+}
+PrintNumber(20);
