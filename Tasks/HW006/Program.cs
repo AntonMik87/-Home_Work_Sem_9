a55b7770b94778e60feca8a0f5f31257957fb23a
«Anton» <hranitel-liks@mail.ru>
2022-09-07 21:02:31 +0700

    add HW006

---
+++ Tasks/HW006/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу возведения числа А в целую стень B
+
+Console.Clear();
+
+int DegreeOfNumber(int numberA, int numberB)
+{
+    if (numberB < 1) return 1;
+    return numberA * (DegreeOfNumber(numberA, numberB - 1));
+}
+
+Console.Write("Введите число A: ");
+int numberA = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine($" Число {numberA} в степени {numberB} = {DegreeOfNumber(numberA, numberB)}");
